82c6959bd783e2a00d1a2637a880d21bf6ad5e52
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-04-03 14:08:03 -0400

    put correct names, added the readme, change font size the the out put

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 function generatePassword(){
 }
-
+// set the lenght of the passwork
 var characterLength = 8;
-var choiceArray = []; //  change the name of this one later
+var optionsArray = []; //  These are the options allowed to select from 
 
-var specialChararray = [ '!','@','#','$','%','^','&','*','[',']','{', '}'];
-var lowerCasearray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upperCasearray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbersArray = ['1','2','3','4','5','6','7','8','9','0'];
+var specialCharacter = [ '!','@','#','$','%','^','&','*','[',']','{', '}'];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ['1','2','3','4','5','6','7','8','9','0'];
 
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); // When the use click this activates the commands
 
 // Write password to the #password input
 function writePassword() {
-  var correctPromts = getPrompts();  //change th is  
+  var selectedPromts = getPrompts();  //this were the promps are selected
   var passwordText = document.querySelector("#password");
   
-    if (correctPromts) {
+    if (selectedPromts) {
   var newPassword = generatePassword();
 
       passwordText.value = newPassword;
   } else{{
-    passwordTest.value = "";
+    passwordText.value = "";
   }
   }
 
@@ -35,8 +35,8 @@ function writePassword() {
   function generatePassword() {
     var password = "";
     for(var i = 0; i < characterLength; i++) {
-    var randomIndex = Math.floor(Math.random() * choiceArray.length);
-    password = password + choiceArray[randomIndex];
+    var randomIndex = Math.floor(Math.random() * optionsArray.length);
+    password = password + optionsArray[randomIndex];
   
   }
     return password;
@@ -46,31 +46,30 @@ function writePassword() {
 
 
 
-
 function getPrompts(){
-   choiceArray = [];
+  optionsArray = [];
 
-    characterLength = parseInt(prompt("How many caharaters do you want your passwor to be? (8*-128 characters"));
+    characterLength = parseInt(prompt("Select between (8 and 12) characters want your passwor to be?"));
 
     if (isNaN (characterLength) || characterLength < 8 || characterLength > 128) { 
-      alert("Character length has to be a number. 8-128 digits. Please try again.");
+      alert("Character length has to be betwen (8 to 128) digits. Please try again!!.");
       return false; 
     }
 
- if (confirm("Would you like lowercase letters in your password")) {
-  choiceArray = choiceArray.concat(lowerCasesarray);
+ if (confirm("Would you like lowercase letters such as: (abcdef) in your password")) {
+  optionsArray = optionsArray.concat(lowerCase);
  } 
 
- if (confirm("Would you like Uppercase letters in your password")) {
-  choiceArray = choiceArray.concat(upperCasearray);
+ if (confirm("Would you like Uppercase letters such as:(ABCDEF) in your password")) {
+  optionsArray = optionsArray.concat(upperCase);
  } 
 
- if (confirm("Would you like numberArray letters in your password")) {
-  choiceArray = choiceArray.concat(numbersArray);
+ if (confirm("Would you like numbers such as: (12345) in your password")) {
+  optionsArray = optionsArray.concat(numbers);
  } 
 
- if (confirm("Would you like specialChararray letters in your password")) {
-  choiceArray = choiceArray.concat(specialChararray);
+ if (confirm("Would you like special characters such as: (!@#$%^) in your password")) {
+  optionsArray = optionsArray.concat(specialCharacter);
  }    
 
  return true;
