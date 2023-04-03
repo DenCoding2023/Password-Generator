d86aa78614a67dde5860c5f58c1461197c265e24
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-04-03 00:01:01 -0400

    changing the position of the function to make it work

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
 function generatePassword(){
+}
 
 var characterLength = 8;
-var choiceArray = [];
+var choiceArray = []; //  change the name of this one later
 
 var specialChararray = [ '!','@','#','$','%','^','&','*','[',']','{', '}'];
 var lowerCasearray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCasearray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbersArray = ['1','2','3','4','5','6','7','8','9','0'];
+
+
+function getPrompts(){
+   choiceArray = [];
+
+    characterLength = parseInt(prompt("How many caharaters do you want your passwor to be? (8*-128 characters"));
+
+    if (isNaN (characterLength) || characterLength < 8 || characterLength > 128) { 
+      alert("Character length has to be a number. 8-128 digits. Please try again.");
+      return false; 
+    }
+
+ if (confirm("Would you like lowecase letters in your password")) {
+  choiceArray = choiceArray.concat(lowerCasesarray);
+ } 
+
+ if (confirm("Would you like Uppercase letters in your password")) {
+  choiceArray = choiceArray.concat(upperCasearray);
+ } 
+
+ if (confirm("Would you like numberArray letters in your password")) {
+  choiceArray = choiceArray.concat(numbersArray);
+ } 
+
+ if (confirm("Would you like specialChararray letters in your password")) {
+  choiceArray = choiceArray.concat(specialChararray);
+ }    
+
+ return true;
+}
 
 
 // Assignment Code
@@ -26,37 +57,8 @@ function writePassword() {
 
 generateBtn.addEventListener("click", writePassword);
 
-
 // console.log(generatePassword(8)); // "F2#m9R$q"
  
 // // funtion to generate passwor//
 
-
-function getPrompts(){
-    characterLength = parseInt(prompt("How many caharaters do you want your passwor to be? (8*-128 characters"));
-
-    if(isNaN (characterLength) || characterLength <8 || characterLength > 128) { 
-      alert("Character length has to be a number. 8-128 digits. Please try again.");
-      return false; 
-    }
- (confirm("Would you like lowecase letters in you pass")) {
-  choiceArray = choiceArray.concat(lowerCasesarray);
- } 
-
- (confirm("Would you like Uppercase letters in you pass")) {
-  choiceArray = choiceArray.concat(upperCasearray);
- } 
-
- (confirm("Would you like numberArray letters in you pass")) {
-  choiceArray = choiceArray.concat(numberArray);
- } 
-
- (confirm("Would you like numberArray letters in you pass")) {
-  choiceArray = choiceArray.concat(numbers   Array);
- }    
-
- return true;
-}
-
-}
 
