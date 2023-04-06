248d426495487aba0ef2b6c31a0d3c549a14adcb
Dennis Luciano <127637377+DenCoding2023@users.noreply.github.com>
2023-04-05 22:09:16 -0400

    added all of the special characters needed.

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ function generatePassword(){
 var characterLength = 8;
 var optionsArray = []; //  These are the options allowed to select from 
 
-var specialCharacter = [ '!','@','#','$','%','^','&','*','[',']','{', '}'];
+var specialCharacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "?", "/", "-", ":", ";", "[", "]", "{", "}", ".", "<", ">", "=", "_", "`", "|", "~"];
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ['1','2','3','4','5','6','7','8','9','0'];
 
 
-// Assignment Code
+// This attached it to the button at Generate
 var generateBtn = document.querySelector("#generate");
 
 
-generateBtn.addEventListener("click", writePassword); // When the use click this activates the commands
+generateBtn.addEventListener("click", writePassword); // When the use click this activates the event listners
 
-// Write password to the #password input
+// This sets the fuction for the use to select the getPropmts options. 
 function writePassword() {
   var selectedPromts = getPrompts();  //this were the promps are selected
   var passwordText = document.querySelector("#password");
@@ -31,12 +31,12 @@ function writePassword() {
   }
 
 
-
+// This is a loop that will continue one to go through the character options. 
   function generatePassword() {
     var password = "";
-    for(var i = 0; i < characterLength; i++) {
+    for(var i = 0; i < characterLength; i++) {   //var is = 0 in order to start for the firt item in the line.
     var randomIndex = Math.floor(Math.random() * optionsArray.length);
-    password = password + optionsArray[randomIndex];
+    password = password + optionsArray[randomIndex];       //this line conccatinate the radomizer and the possible optiosn.
   
   }
     return password;
@@ -45,11 +45,11 @@ function writePassword() {
 }
 
 
-
+//Here we set up the chriteria for the promps to pop up and provide some feedback. 
 function getPrompts(){
-  optionsArray = [];
+  optionsArray = []; //These are the if statement tha provide feedback to the user. I also provided exampples in order to guide the user
 
-    characterLength = parseInt(prompt("Select between (8 and 12) characters want your passwor to be?"));
+    characterLength = parseInt(prompt("Select between (8 and 128) characters want your passwor to be?")); // this is were we set the perameters selectons
 
     if (isNaN (characterLength) || characterLength < 8 || characterLength > 128) { 
       alert("Character length has to be betwen (8 to 128) digits. Please try again!!.");
@@ -76,13 +76,5 @@ function getPrompts(){
 }
 
 
-
-// Add event listener to generate button
-
-
-
-// console.log(generatePassword(8)); // "F2#m9R$q"
- 
-// // funtion to generate passwor//
 
 
